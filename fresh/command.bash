68cfeb37da62aede1bdc290a405710b098adf754
Alex Carlin <carlin@ucdavis.edu>
2014-09-29 13:12:26 -0700

    Cleaned up

--- fresh/command.bash
+++ fresh/command.bash
@@ -1,1 +1,3 @@
 rosetta_scripts.macosclangrelease --parser::protocol design-off.xml -s tom-v106f.pdb @enzdes.flags
+rosetta_scripts.macosclangrelease --parser::protocol design-off.xml -s tom-a113f.pdb @enzdes.flags
+rosetta_scripts.macosclangrelease --parser::protocol design-off.xml -s tom-wt.pdb    @enzdes.flags
